57fefb51f9b63cdd973ba676b3eacf9bbf080f87
Kershan <n.kershan@gmail.com>
2014-08-14 22:37:56 +0200

    Added new classes to be coded.

---
+++ Manager/Season.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Manager
+{
+    class Season
+    {
+        private string name;
+        private bool complete;
+        private int id, collectionId, episodes;
+    }
+}
